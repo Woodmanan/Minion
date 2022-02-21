0c9d8bed3fbb555224cdfef8a85ae364983d6128
Joey Perrino <epiccrashv3@gmail.com>
2022-02-20 20:11:40 -0500

    Skill Tree Implementation Begun
    - Skill Tree object exists, with test connections
    - Skill SO added - effect is placeholder, but the other attributes can stay
      - Test of this added
    - Skill Node added for Skill Tree; uses whether any previous skills were unlocked, Skill SOs, and Skill level as inputs (possibly allows for skill stacking)
      - Of these, Skills should be the only thing we ever manually set
      - Output is purchase status
    - Untested

---
+++ RoguelikeFramework/Assets/Scripts/Skills/Skill Tree/SkillNode.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public class SkillNode : Node
+{
+
+	[Input] public bool canBePurchased;
+	[Input] public Skill[] skills;
+	[Input] public int skillLevel;
+	[Output] public bool hasBeenPurchased;
+
+	// Use this for initialization
+	protected override void Init()
+	{
+		base.Init();
+
+	}
+
+	// Return the correct value of an output port when requested
+	public override object GetValue(NodePort port)
+	{
+		if (port.fieldName == "canBePurchased")
+		{
+			foreach (bool hasBeenPurchased in GetInputValues("canBePurchased", canBePurchased))
+            {
+				if (hasBeenPurchased) return true;
+            }
+			return false;
+		}
+		else if (port.fieldName == "skills") return skills;
+		else if (port.fieldName == "skillLevel") return skillLevel;
+		else if (port.fieldName == "hasBeenPurchased") return hasBeenPurchased;
+		else return null; // Crisis mode, everything is broken, this should never happen
+	}
+
+	public bool IsConnectedTo(NodePort port)
+	{
+		return IsConnectedTo(port);
+	}
+
+	public Object GetCurrentSkill()
+	{
+		if (skillLevel > skills.Length) return null;
+		else return skills[skillLevel];
+	}
+}
